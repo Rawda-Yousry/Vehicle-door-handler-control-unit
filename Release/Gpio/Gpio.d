01681a0f9c247c85f59cc41c9400ad83ae0e3b61
Rawda-Yousry <rawda.ahmed02@eng-st.cu.edu.eg>
2023-06-26 17:40:04 +0300

    delete unwanted

--- Release/Gpio/Gpio.d
+++
@@ -1,9 +0,0 @@
-Gpio/Gpio.o: ../Gpio/Gpio.c ../Gpio/Gpio.h \
- D:\eclipse-workspace\TryProject\Lib/Std_Types.h ../Gpio/Gpio_Private.h \
- D:\eclipse-workspace\TryProject\Lib/Utils.h \
- D:\eclipse-workspace\TryProject\Lib/Std_Types.h
-../Gpio/Gpio.h:
-D:\eclipse-workspace\TryProject\Lib/Std_Types.h:
-../Gpio/Gpio_Private.h:
-D:\eclipse-workspace\TryProject\Lib/Utils.h:
-D:\eclipse-workspace\TryProject\Lib/Std_Types.h:
